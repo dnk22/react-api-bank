9d5ff6d37ca8833838af4b0c64d8a3e5cef82542
Duy Nguyen <duynk198@gmail.com>
2023-05-18 00:45:39 +0700

    fix : auth

--- src/app/layout/index.tsx
+++ src/app/layout/index.tsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function LayoutCommon() {
+  const navigate = useNavigate();
+  const isLogged = localStorage.getItem('token');
+
+  useEffect(() => {
+    if (!isLogged) {
+      navigate('/auth/login');
+    }
+  }, [isLogged, navigate]);
   return <div>layoy</div>;
 }
 export default LayoutCommon();
--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,22 +1,12 @@
-import { Route, Routes, useNavigate } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import LayoutCommon from '../layout/index';
 
 import Login from '../features/Auth/Login';
 import Register from '../features/Auth/Register';
 import Account from '../features/Account';
 import ForgotPassword from 'features/Auth/ForgotPassword';
-import { useEffect } from 'react';
 
 function AppRouter() {
-  const navigate = useNavigate();
-  const isLogged = localStorage.getItem('token');
-
-  useEffect(() => {
-    if (!isLogged) {
-      navigate('/auth/login');
-    }
-  }, [isLogged, navigate]);
-
   return (
     <Routes>
       <Route path="/auth/login" element={<Login />} />
