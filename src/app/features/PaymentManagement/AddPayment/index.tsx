91ade8c0b2b06dd9f69e01c72a3772f6fa92d577
Duy Nguyen <duynk198@gmail.com>
2023-07-21 21:22:57 +0700

    update

--- src/app/features/PaymentManagement/AddPayment/index.tsx
+++ src/app/features/PaymentManagement/AddPayment/index.tsx
@@ -1,6 +1,23 @@
 import React from 'react';
+import { Box, Grid, Modal, Stack } from '@mui/material';
 
-function AddPayment() {
-  return <div className="modal"></div>;
+function AddPayment({ handleClose }) {
+  return (
+    <Modal
+      open={true}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Stack spacing={2}>
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          <Grid item xs={6}>
+            User Name
+          </Grid>
+          <Grid item xs={6}></Grid>
+        </Grid>
+      </Stack>
+    </Modal>
+  );
 }
 export default AddPayment;
